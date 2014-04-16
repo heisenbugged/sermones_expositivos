2bcc323e9f083f609ab937648e9abc1c033703d8
James Strong <laspluviosillas@gmail.com>
2014-04-15 20:06:57 -0500

    Add ability to edit sermons.
    condense categories
    sort categories alphabetically

--- app/controllers/sermons_controller.rb
+++ app/controllers/sermons_controller.rb
@@ -9,6 +9,7 @@ class SermonsController < ApplicationController
       @categories = Sermon.unscoped.group('category')
                           .select('category, COUNT(id) as sermons_count')
                           .where('category IS NOT NULL')
+                          .order('category asc')
     end
 
   end
@@ -17,6 +18,19 @@ class SermonsController < ApplicationController
     @sermon = Sermon.new
     render layout: 'application'
   end
+
+  def edit
+    @sermon = Sermon.find params[:id]
+    render layout: 'application'
+  end
+
+  def update
+    @sermon = Sermon.find params[:id]
+    flash[:notice] = "Sermon saved successfully" if @sermon.update_attributes(sermon_params)
+    respond_with(@sermon, location: sermons_path)
+  end
+
+
 
   def create
     @sermon = Sermon.new sermon_params
@@ -27,7 +41,7 @@ class SermonsController < ApplicationController
   private
 
   def sermon_params
-    params[:sermon].permit(:title, :author, :audio, :date)
+    params[:sermon].permit(:title, :author, :audio, :date, :category)
   end
 
 end
